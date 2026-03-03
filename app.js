3d240bc2e003e61d606f847b83725d702fc869f4
Dani <danilago1998@gmail.com>
2026-03-03 19:44:12 +0000

    Añadiendo comentarios

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// ============================
+// ===== INSTANCIAS
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,9 +12,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+
+// ============================
+// ===== CONFIGURACIONES
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
+// ============================
+// ===== MIDDLEWARES
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +29,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// ============================
+// ===== RUTAS
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
+// ============================
+// ===== MANEJADORES DE ERRORES
+
+// Captura de errores 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// ============================
+// MANEJADOR DE ERRORES
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+// ============================
+// ===== EXPORTACIÓN DE LA APP
 
 module.exports = app;
